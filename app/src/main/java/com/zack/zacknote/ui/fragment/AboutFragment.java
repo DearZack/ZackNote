22cfb4158e24eed82bd14c1fcaf9c6ab3ec3d1b1
DearZack <zack4work@gmail.com>
2016-06-09 15:45:47 +0800

    finish

--- app/src/main/java/com/zack/zacknote/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/zack/zacknote/ui/fragment/AboutFragment.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,8 @@ import com.zack.zacknote.R;
  */
 public class AboutFragment extends Fragment {
 
+    private TextView personalAddress, githubAddress, githubIssues;
+
     public AboutFragment() {
     }
 
@@ -24,6 +28,27 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_app, container, false);
+        personalAddress = (TextView) view.findViewById(R.id.about_app_personal_address);
+        githubAddress = (TextView) view.findViewById(R.id.about_app_github_address);
+        githubIssues = (TextView) view.findViewById(R.id.about_app_github_issues);
+        String personal = "作者Zack：";
+        String address = "项目地址：";
+        String issues = "反馈建议：";
+        personal += "<a href='https://github.com/DearZack'>GitHub</a> ";
+        personal += "<a href='http://dearzack.github.io' >Blog</a> ";
+        personal += "<a href='http://weibo.com/2867897514/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1' >微博</a> ";
+        personal += "<a href='https://www.zhihu.com/people/zhou-shi-kong' >知乎</a> ";
+        address += "<a href='https://github.com/DearZack/ZackNote'>ZackNote</a>";
+        issues += "<a href='https://github.com/DearZack/ZackNote/issues'>Issues</a>";
+        CharSequence personalCharSequence = Html.fromHtml(personal);
+        personalAddress.setText(personalCharSequence);
+        personalAddress.setMovementMethod(LinkMovementMethod.getInstance());
+        CharSequence addressCharSequence = Html.fromHtml(address);
+        githubAddress.setText(addressCharSequence);
+        githubAddress.setMovementMethod(LinkMovementMethod.getInstance());
+        CharSequence issuesCharSequence = Html.fromHtml(issues);
+        githubIssues.setText(issuesCharSequence);
+        githubIssues.setMovementMethod(LinkMovementMethod.getInstance());
         return view;
     }
 }
